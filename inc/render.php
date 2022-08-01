1b8db8af280bd09254f9b83c73817fc8e22caa39
Joe Rouse <joe_rouse@hotmail.co.uk>
2022-08-01 12:40:18 +0100

    clear repo

--- inc/render.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-function render_gutenberg_test_block() {
-  $wrapper_attributes = get_block_wrapper_attributes();
-  return sprintf('<p %1$s>Test</p>', $wrapper_attributes);
-}
--- src/components/edit.js
+++
@@ -1,7 +0,0 @@
-const { useBlockProps } = wp.blockEditor;
-
-const edit = () => {
-  const blockProps = useBlockProps();
-  return <div {...blockProps}>My Test Block</div>;
-};
-export default edit;
